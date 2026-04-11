286566d9b118a3cfb6fd07b9f7122a07fa606056
Olaminiyi <olaiyaolaminiyi@gmail.com>
2026-04-12 00:33:02 +0100

    Update MovieBookingController.java

--- src/main/java/com/moviebooking/moviebooking/controller/MovieBookingController.java
+++ src/main/java/com/moviebooking/moviebooking/controller/MovieBookingController.java
@@ -13,6 +13,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
+
 @RestController
 @RequestMapping("/api/v1/seat-bookings")
 @AllArgsConstructor
